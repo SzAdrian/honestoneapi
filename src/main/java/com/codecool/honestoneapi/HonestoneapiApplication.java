1dade97ba7841e0218cecddad2df5015cf41e061
SzAdrian <51090249+SzAdrian@users.noreply.github.com>
2020-03-09 11:09:02 +0100

    Initial commit

---
+++ src/main/java/com/codecool/honestoneapi/HonestoneapiApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.honestoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonestoneapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HonestoneapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/honestoneapi/HonestoneapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.honestoneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonestoneapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
