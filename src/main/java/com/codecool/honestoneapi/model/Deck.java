3be7e4b09d652ff797f92260d300fffd48459cda
SzAdrian <51090249+SzAdrian@users.noreply.github.com>
2020-03-12 09:46:02 +0100

    import deleted

--- src/main/java/com/codecool/honestoneapi/model/Deck.java
+++ src/main/java/com/codecool/honestoneapi/model/Deck.java
@@ -1,15 +1,47 @@
 package com.codecool.honestoneapi.model;
 
-import com.intellij.openapi.vcs.history.VcsRevisionNumber;
-
-import java.util.List;
-
 public class Deck {
     private String deckcode;
     private Integer hero;
     private Integer format;
     private String name;
     private Integer userId;
+
+    public String getDeckcode() {
+        return deckcode;
+    }
+
+    public void setDeckcode(String deckcode) {
+        this.deckcode = deckcode;
+    }
+
+    public Integer getHero() {
+        return hero;
+    }
+
+    public void setHero(Integer hero) {
+        this.hero = hero;
+    }
+
+    public Integer getFormat() {
+        return format;
+    }
+
+    public void setFormat(Integer format) {
+        this.format = format;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
 
     public Integer getUserId() {
         return userId;
