5bba50532d5335ba369acb5210a3958a62b3017c
Paebo <petibors2@gmail.com>
2020-03-26 10:12:51 +0100

    "Merge to master"

--- src/main/java/com/codecool/honestoneapi/dao/DeckStorage.java
+++ src/main/java/com/codecool/honestoneapi/dao/DeckStorage.java
@@ -1,33 +1,27 @@
 package com.codecool.honestoneapi.dao;
 
 import com.codecool.honestoneapi.model.Deck;
-import com.codecool.honestoneapi.model.Usr;
 import com.codecool.honestoneapi.repository.DeckRepository;
-import com.codecool.honestoneapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Repository
 public class DeckStorage {
 
     @Autowired
-    private DeckRepository deckRepository;
+    DeckRepository deckRepository;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    public void saveDeck(Deck deck, Long userId) {
-        Usr user = userRepository.findById(userId).get();
-        user.addDeck(deck);
-        userRepository.save(user);
+    public void saveDeck(Deck deck) {
+        deckRepository.save(deck);
     }
-
-    public List<Deck> getDecksByUserId(Integer userId) {
-        return deckRepository.findByUserId(Long.valueOf(userId));
-    }
+//
+//    public List<Deck> getDecksByUserId(Integer userId) {
+//        return deckStorage.stream()
+//                .filter(deck -> deck.getUserId() == userId)
+//                .collect(Collectors.toList());
+//    }
 }
---
+++ src/main/java/com/codecool/honestoneapi/dao/GenerationStorage.java
@@ -0,0 +1,4 @@
+package com.codecool.honestoneapi.dao;
+
+public class GenerationStorage {
+}
